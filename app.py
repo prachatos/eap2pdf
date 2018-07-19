7f1cbaeed0b0ace46dca841016064be6c8634173
Prachatos Mitra <prachatosmitra@gmail.com>
2018-07-19 17:44:57 +0530

    Use multiprocessor to parallely download books

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import multiprocessing
 import os
 import sys
 import time
 import urllib.request
 from urllib.error import HTTPError
 from fpdf import FPDF
-from PyPDF2 import PdfFileWriter, PdfFileReader
+import PyPDF2
+
 
 class EAPBookFetch:
 
@@ -12,6 +14,8 @@ class EAPBookFetch:
     EAP_FILENAME = 'default.jpg'
     DEFAULT_HEIGHT = 1200
     DEFAULT_WIDTH = 1200 * 0.8
+    JPEG_PATH = 'jpgs'
+    PDF_PATH = 'pdfs'
 
     @staticmethod
     def join_url(*args):
@@ -27,19 +31,21 @@ class EAPBookFetch:
         else:
             return 0
 
-    def download_jpg(self):
-        base_eap = self.url.split('/')[0]
-        eap_url_for_entry = self.url.replace('/', '_')
+    def download_jpg(self, url):
+        base_eap = url.split('/')[0]
+        eap_url_for_entry = url.replace('/', '_')
         combined_url = self.join_url(self.EAP_BASE_URL, base_eap, eap_url_for_entry)
         pg = 1
         can_go = True
         file_list = []
+        if not os.path.exists(self.JPEG_PATH):
+            os.makedirs(self.JPEG_PATH)
         while can_go:
-            dl_url = self.join_url(combined_url, str(pg) + '.jp2', 'full', str(self.height) + ',' + str(self.DEFAULT_WIDTH),
-                                   str(self.rotation), self.EAP_FILENAME + '?t=' + str(int(time.time() * 1000)))
-            if not os.path.exists('jpgs'):
-                os.makedirs('jpgs')
-            title = os.path.join('jpgs', eap_url_for_entry + '_' + str(pg) + '.jpg')
+            dl_url = self.join_url(combined_url, str(pg) + '.jp2', 'full', str(self.height) + ',' +
+                                   str(self.DEFAULT_WIDTH), str(self.rotation),
+                                   self.EAP_FILENAME + '?t=' + str(int(time.time() * 1000)))
+
+            title = os.path.join(self.JPEG_PATH, eap_url_for_entry + '_' + str(pg) + '.jpg')
             pg = pg + 1
             print('Downloading ' + title)
             try:
@@ -62,13 +68,13 @@ class EAPBookFetch:
             else:
                 pdf.image(image, h=self.height, w=self.DEFAULT_WIDTH)
         page_count = pdf.page_no()
-        if not os.path.exists('pdfs'):
-            os.makedirs('pdfs')
-        pdf.output(os.path.join('pdfs', eap_url_for_entry + '.pdf'))
+        if not os.path.exists(self.PDF_PATH):
+            os.makedirs(self.PDF_PATH)
+        pdf.output(os.path.join(self.PDF_PATH, eap_url_for_entry + '.pdf'))
         if page_count > len(file_list):
             # delete pg 1
-            infile = PdfFileReader(os.path.join('pdfs', eap_url_for_entry + '.pdf'))
-            outfile = PdfFileWriter()
+            infile = PyPDF2.PdfFileReader(os.path.join(self.PDF_PATH, eap_url_for_entry + '.pdf'))
+            outfile = PyPDF2.PdfFileWriter()
             pg = 1
             print('Deleting blank page...')
             while pg < page_count:
@@ -76,10 +82,10 @@ class EAPBookFetch:
                 print(pg)
                 pg = pg + 1
                 outfile.addPage(p)
-            with open(os.path.join('pdfs', eap_url_for_entry + '_nofirst.pdf'), 'wb') as f:
+            with open(os.path.join(self.PDF_PATH, eap_url_for_entry + '_nofirst.pdf'), 'wb') as f:
                 outfile.write(f)
             try:
-                os.remove(os.path.join('pdfs', eap_url_for_entry + '.pdf'))
+                os.remove(os.path.join(self.PDF_PATH, eap_url_for_entry + '.pdf'))
             except OSError:
                 pass
 
@@ -87,21 +93,24 @@ class EAPBookFetch:
         if len(sys.argv) < 2:
             raise Exception("No URL to download")
         else:
-            self.url = sys.argv[1]
             # shitty code, sorry
-            if len(sys.argv) == 3:
-                self.rotation = self.set_rotate(sys.argv[2])
-            if len(sys.argv) == 4:
-                self.height = sys.argv[3]
-            if len(sys.argv) == 5:
-                self.type = str(sys.argv[4])
-        self.download_jpg()
+            if len(sys.argv) < 3:
+
+                url = sys.argv[1]
+                self.download_jpg(url)
+            else:
+                urls = []
+                for arg in sys.argv:
+                    if arg.find("EAP") != -1:
+                        urls.append(arg)
+                pool = multiprocessing.Pool(processes=len(sys.argv) - 1)
+                pool.map(self.download_jpg, urls)
 
     def __init__(self):
-        self.url = ''
         self.rotation = 0
         self.height = self.DEFAULT_HEIGHT
         self.type = 'p'  # probably broken for landscape
 
 
-EAPBookFetch().run()
+if __name__ == '__main__':
+    EAPBookFetch().run()
